3e4751a9b7ca81eb4c53f67d3f489310eb1504b5
Valentino D <57812282+ValentinooD@users.noreply.github.com>
2024-07-03 23:23:53 +0200

    Fix bug to make loginSession bean Session Scoped

--- WebFrontEnd/src/main/java/vua/oicar/webfrontend/Beans.java
+++ WebFrontEnd/src/main/java/vua/oicar/webfrontend/Beans.java
@@ -2,11 +2,13 @@ package vua.oicar.webfrontend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.SessionScope;
 import vua.oicar.webfrontend.models.LoginSession;
 
 @Configuration
 public class Beans {
     @Bean
+    @SessionScope
     public LoginSession loginSession() {
         return new LoginSession();
     }
